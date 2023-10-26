047caa736d0a17c571dabf3d8a91f77c2837b326
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-10-26 17:22:10 +0200

    add: readme file corrected js file html file

--- js/ex2script.js
+++ js/ex2script.js
@@ -6,47 +6,48 @@ let pcNumber = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
 let choseOddEven = false; // if the user chose odd or even
 let choseNumber = false; // if the user chose a number
 
-do{
+while (choseOddEven === false){
     userChoice = prompt("Choose between 'odd' and 'even'");
     if(userChoice === "even" || userChoice === "odd"){
         choseOddEven = true;
-        console.log(pcNumber, choseOddEven);
+        console.log(pcNumber);
     }
+}
 
-} while (choseOddEven === false);
-
-do{
+while (choseNumber === false){
     userNumber = parseInt(prompt("Choose a number between 1 and 5"));
-    if(!isNaN(userNumber) || userNumber >= 1 || userNumber <= 5){
+    if(!isNaN(userNumber) && userNumber >= 1 && userNumber <= 5){
         choseNumber = true;
-        console.log(userNumber, choseNumber);
+        console.log(userNumber);
     }
+}
 
-} while (choseNumber === false);
+const winner = returnWinner(userChoice, userNumber, pcNumber);
+alert(winner);
 
-winner = returnWinner(userChoice, userNumber);
-
-function returnWinner(funcOddEven, funcNumber){
+function returnWinner(funcChoice, funcNumber, funcPcNumber){
 
     let sum;
-    sum = funcNumber + pcNumber;
+    sum = funcNumber + funcPcNumber;
     console.log(sum);
+    
     let result = "";
-    console.log(result);
-    let winner = "";
-
 
     if (sum % 2 === 0){
         result = "even";
     } else{
         result = "odd";
     }
+    
+    console.log(result);
 
-    if (result === funcOddEven){
+    let winner = "";
+
+    if (result === funcChoice){
         winner = "You win";
     } else {
         winner = "You lose";
     }
-
+    console.log(winner);
     return winner;
 }
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Exercises 26 / 10 / 2023
+<!-- Even vs. Odd  -->
+## Exercise 2
+
+The program asks the user to choose between "odd" and "even", and to choose a number between 1 and 5.
+Then it generates a random number between 1 and 5 for the computer.
+In the end, the program sums the numbers and calculates if the sum is odd or even, and returns if the player won or not based on their choice.
+
+### Solution
+
+#### Info
+
+1. Get the choice between odd and even from the user;
+2. Get the user number;
+3. Get the pc number.
+
+#### Procedure
+
+1. Sum the two numbers;
+2. Check if the sum is an odd or even number.
+
+#### Output
+
+Print "You win" if the user guesses if the sum was odd or even correctly, or print "You lose" if not.
