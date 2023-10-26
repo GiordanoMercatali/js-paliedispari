177b634020e613aed22b93a2b152fe25752cbbbc
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-10-26 18:22:31 +0200

    add: ex 1 completed

---
+++ js/ex1script.js
@@ -0,0 +1,35 @@
+// Palindrome
+
+let userWord = ""; // type a word
+let choseWord = false;
+let reversedWord = "";
+
+while (choseWord === false){
+    userWord = prompt("Type a word");
+    choseWord = true;
+    console.log(`Your word is: ${userWord}`);
+}
+
+
+
+reverseResult = reverseString(userWord);
+
+const isPalindrome = returnPalindrome(userWord, reversedWord);
+console.log(isPalindrome);
+
+function reverseString(stringToReverse, stringBeingReversed){
+    for(let i = stringToReverse.length - 1; i >= 0; i--){
+        reversedWord += stringToReverse[i];
+    }
+    console.log(reversedWord);
+    return reversedWord;
+}
+
+function returnPalindrome(funcWord, funcWordR){
+    let isPalindrome = false;
+
+    if(funcWord === funcWordR){
+        isPalindrome = true;
+    }
+    return isPalindrome;
+}
